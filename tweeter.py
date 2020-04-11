0c6a2311d135757a94f503d0a713c03712e76b28
Daniel Lenshin <56211169+its-baniel-boys@users.noreply.github.com>
2020-04-10 22:05:43 -0400

    Added Blacklist filtering to tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -2,6 +2,14 @@ from twython import Twython
 import json
 import random
 
+#Checks an extension with the blacklist array
+def isBlacklisted(extension):
+    for element in blacklist:
+        if extension == element:
+            return True
+    
+    return False
+
 #Account verificaton
 API_key = 'YOUR API KEY HERE'
 API_secret_key = 'YOUR SECRET API KEY HERE'
@@ -10,11 +18,19 @@ Access_token_secret = 'YOUR SECRET ACCESS TOKEN HERE'
 UArticlesBot = Twython(API_key, API_secret_key, Access_token, Access_token_secret)
 
 articles = json.loads(open('UAList.json').read()) #Import array from json file
+blacklist = json.loads(open('Blacklist.json').read()) #Import blacklist array
 randomArticle = random.randrange(0, (len(articles)/2), 2) #Select random extension from array
 
 #Get article extension and title using random article number
 articleExtension = articles[randomArticle]
 articleTitle = articles[randomArticle + 1]
+
+#Checks articleExtension with blacklist array, and chooses another element if extension is blacklisted
+while isBlacklisted(articleExtension):
+    #Redefine article extension and title
+    randomArticle = random.randrange(0, (len(articles)/2), 2)
+    articleExtension = articles[randomArticle]
+    articleTitle = articles[randomArticle + 1]
 
 #Send tweet
 UArticlesBot.update_status(status = 'Today\'s article is: %s\nen.wikipedia.org/wiki/%s' %(articleTitle, articleExtension))
